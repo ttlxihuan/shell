9230eb1f6be1993ad5ab23e32edba7cbd86450f9
huan.xi <huan.xi@broadshare.cn>
2020-09-25 14:16:36 +0800

    说明文案调整

--- basic.sh
+++ basic.sh
@@ -541,8 +541,8 @@ init_install (){
     # 加载环境配置
     source /etc/profile
     # 内存空间不够
-    if if_version `free -tg|tail -1|grep -oP '\d+'|head -1` '<' '3';then
-        dd if=/dev/zero of=/swap bs=1024 count=1M
+    if ! free -h|grep -qi swap && if_version `free -tg|tail -1|grep -oP '\d+'|head -1` '<' '3';then
+        dd if=/dev/zero of=/swap bs=1024 count=2M
         mkswap /swap
         swapon /swap
         echo "/swap swap swap sw 0 0" >> /etc/fstab
