ebbabf89fe98579c5549f2ef090952a1e1c54a0f
huan.xi <huan.xi@broadshare.cn>
2021-12-07 15:10:36 +0800

    密码处理，内存处理，错误

--- installs/database/redis-install.sh
+++ installs/database/redis-install.sh
@@ -24,18 +24,28 @@ DEFINE_INSTALL_TYPE='make'
 # 定义安装参数
 DEFINE_INSTALL_PARAMS="
 [-s, --save='60 5']开启自动保存到硬盘，配置规则：n秒 m次修改，为空则不开启
-[-p, --password='']安装成功后修改的新密码，默认或为空时随机生成25位密码
+[-p, --password='']安装成功后授权密码
+#为空即无密码
+#随机生成密码 %num，比如：%10
+#指定固定密码，比如：123456
 [-c, --cluster-hosts='']指定集群地址名集 ip:port，端口号不传为默认6379
-#多个使用逗号分开，最少三个含当前服务器，当前IP可以不传创建时自动补充
-#只有5.0及以上的版本有效
+#多个使用逗号分开，最少三个含当前服务器
+#当前IP可以不传创建时自动补充
+#此选项只有5.0及以上的版本有效
 [-R, --cluster-replicas=1]指定集群副本数
-[-m, --max-memory=-1]指定配置最大内存字节数，如果不设置可能会导致内存不足机器死机。
-#< 0 或空为即可用内存的70%，
-#= 0 即不配置
-#> 0 即配置指定大小
+[-a, --cluster-masterauth='']指定集群通信密码
+#为空即无密码
+#随机生成密码 %num，比如：%10
+#指定固定密码，比如：123456
+[-m, --max-memory='']指定配置服务运行最大占用内存（整数）
+#为空即默认可用内存的70%
+#指定可用内存占比，比如：70%
+#指定对应的大小，单位（B,K,M,G,T），比如：4G
+#不指定单位为B
+#指定为0时即不配置内存
 "
 # 加载基本处理
-source $(realpath ${BASH_SOURCE[0]}|sed -r 's/[^\/]+$//')../../includes/install.sh || exit
+source $(cd $(dirname ${BASH_SOURCE[0]}); pwd)/../../includes/install.sh || exit
 # 初始化安装
 init_install '3.0.0' "http://download.redis.io/releases/" 'redis-\d+\.\d+\.\d+\.tar\.gz'
 # 集群限制
@@ -53,13 +63,16 @@ if [ -n "$ARGV_cluster_hosts" ];then
     if ! printf '%s' "$ARGV_cluster_replicas"|grep -qP '^[1-9]\d+$';then
         error_exit '--cluster-replicas 集群副本数必需是正整数'
     fi
+    parse_use_password REDIS_MASTERAUTH_PASSWORD "$ARGV_cluster_masterauth"
 fi
 if [ -n "$ARGV_save" ] && ! printf '%s' "$ARGV_save"|grep -qP '^\s*([1-9]\d*|\d)\s+([1-9]\d*|\d)\s*$';then
     error_exit '--save 开启自动保存到硬盘参数错误'
 fi
-if [ -n "$ARGV_max_memory" ] && printf '%s' "$ARGV_max_memory"|grep -qP '^-?[1-9]\d*$';then
-    error_exit '--max-memory 指定配置最大内存字节数错误'
+# 解析最大运行内存参数处理
+if ! parse_use_memory REDIS_MAX_MEMORY "${ARGV_max_memory:-70%}";then
+    error_exit '--max-memory 指定错误值'
 fi
+parse_use_password REDIS_PASSWORD "$ARGV_password"
 memory_require 4 # 内存最少G
 work_path_require 1 # 安装编译目录最少G
 install_path_require 1 # 安装目录最少G
@@ -69,7 +82,7 @@ download_software http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
 
 # 新版的redis需要更高的GCC
 if if_version "$REDIS_VERSION" ">=" "6.0.0" && if_version "`cc --version|grep -oP '\d+(\.\d+){2}'|head -1`" "<" "5.1.0";then
-    run_install_shell gcc-install.sh 7.5.0
+    run_install_shell gcc 7.5.0
     if_error 'install gcc fail'
 fi
 
@@ -89,17 +102,9 @@ cd $INSTALL_PATH$REDIS_VERSION
 info_msg 'redis 配置文件修改'
 sed -i -r 's/^(daemonize )no/\1yes/' redis.conf # 后台运行
 # 最大内存，如果不设置可能会导致内存不足机器死机，默认配置为可用内存的70%
-if (( ARGV_max_memory > 0 ));then
-    REDIS_MAX_MEMORY=ARGV_max_memory
-else
-    # 内核3.14的上有MemAvailable
-    if grep -q '^MemAvailable:' /proc/meminfo;then
-        REDIS_MAX_MEMORY=$(cat /proc/meminfo|grep -P '^(MemFree|MemAvailable):'|awk '{count+=$2} END{printf "%d",count * 0.7}')
-    else
-        REDIS_MAX_MEMORY=$(cat /proc/meminfo|grep -P '^(MemFree|Buffers|Cached):'|awk '{count+=$2} END{printf "%d",count * 0.7}')
-    fi
+if (( REDIS_MAX_MEMORY > 0 ));then
+    sed -i -r "s/^(\s*#)?\s*(maxmemory )/\2${REDIS_MAX_MEMORY}/" redis.conf
 fi
-sed -i -r "s/^(\s*#)?\s*(maxmemory )/\2${REDIS_MAX_MEMORY}/" redis.conf
 # 最大连接数据
 # sed -i -r "s/^(\s*#)?\s*(maxclients )/\220000/" redis.conf
 # 开启自动保存到硬盘持久化配置
@@ -107,15 +112,18 @@ if [ -n "$ARGV_save" ];then
     sed -i -r "s/^#\s*(save )\s*\d+.*/\1 $ARGV_save/" redis.conf # 开启60秒内有50次修改自动保存到硬盘
 fi
 sed -i -r 's/^(dir ).\//\1.\/data/' redis.conf # 数据在磁盘保存目录
-
 # 密码设置
-sed -i -r 's/^#\s*(requirepass ).\//\1 这里是密码内容/' redis.conf # 设置用户连接授权密码
-
+if [ -n "$REDIS_PASSWORD" ];then
+    sed -i -r 's/^#\s*(requirepass ).\//\1'${REDIS_PASSWORD}'/' redis.conf # 设置用户连接授权密码
+fi
 # 集群设置
-sed -i -r 's/^#\s*(cluster-enabled ).*/\1yes/' redis.conf # 开启集群
-sed -i -r 's/^#\s*(cluster-node-timeout ).*/\15000/' redis.conf # 集群节点连接超时毫秒数
-sed -i -r 's/^#\s*(masterauth ).*/\1 这里是密码内容/' redis.conf # 设置集群连接授权密码
-
+if [ -n "$ARGV_cluster_hosts" ];then
+    sed -i -r 's/^#\s*(cluster-enabled ).*/\1yes/' redis.conf # 开启集群
+    sed -i -r 's/^#\s*(cluster-node-timeout ).*/\15000/' redis.conf # 集群节点连接超时毫秒数
+    if [ -n "$REDIS_MASTERAUTH_PASSWORD" ];then
+        sed -i -r 's/^#\s*(masterauth ).*/\1'${REDIS_MASTERAUTH_PASSWORD}'/' redis.conf # 设置集群连接授权密码
+    fi
+fi
 mkdirs data redis
 
 # 启动服务
