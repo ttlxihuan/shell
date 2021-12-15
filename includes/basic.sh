c9ee09305df37b74e317bacca9edabe96245c266
huan.xi <huan.xi@broadshare.cn>
2021-12-15 21:03:01 +0800

    脚本参数增加验证

--- includes/basic.sh
+++ includes/basic.sh
@@ -39,9 +39,7 @@ mkdirs(){
 #                           支持运算：+-*/^%
 # return 1|0
 math_compute(){
-    if ! if_command bc; then
-        packge_manager_run install bc
-    fi
+    tools_install bc
     local SCALE_NUM=0
     if [ -n "$3" ]; then
         SCALE_NUM=$(echo "$3"|grep -oP '^\d+'|awk '{if($1 == ""){print "0"}else{print $1}}')
@@ -175,6 +173,16 @@ ask_permit(){
     local ASK_INPUT
     ask_select ASK_INPUT "$1" || [ "$ASK_INPUT" = 'y' -o "$ASK_INPUT" = 'Y' ]
     return $?
+}
+# 获取文件系统的信息
+# 此命令主要是处理很长的存储名可能会换行导致awk处理错位
+# 虚拟机常见，通过修正可以让存储名与对应的信息保持在一行中
+# @command df_awk $options
+# @param $options         选项参数
+# return 1|0
+df_awk(){
+    #去掉首行标题，合并截断行
+    df $@|awk '{if(NR > 1){if(NF==1){prev=$1}else{{print prev,$1,$2,$3,$4,$5,$6,$7}prev=""}}}'
 }
 # 包管理系统运行
 # @command packge_manager_run $command $packge_name
@@ -341,21 +349,33 @@ if_version(){
     fi
     return 1;
 }
-# 解析命令参数
+# 解析shell脚本参数
 # 命令参数解析成功后将写入参数名规则：
 #   参数： ARGU_参数全名
 #   选项： ARGV_选项全名
 # 横线转换为下划线，缩写选项使用标准选项代替，使用时注意参数名的大小写不变
-# @command parse_command_param $define_name $options_name
+# @command parse_shell_param $define_name $options_name
 # @param $define_name       定义参数变量名
 #                           配置参数规则：
-#                           [name]              定义参数
-#                           [-n, --name]        定义无值选项
-#                           [-n, --name='']     带选项值的选项
+#                           [name, {validate}]              定义参数
+#                           [name='', {validate}]           定义参数带默认值
+#                           [-n, --name, {validate}]        定义无值选项
+#                           [-n, --name='', {validate}]     带选项值的选项
+#                           说明：
+#                           name        参数名，前缀无减号，组合要求：字母、数字、减号
+#                           -n          短选项名，前缀一个减号，组合要求：字母、数字、减号
+#                                       短选项必需定义在长选项名前面且不可单独定义短选项
+#                           --name      长选项名，前缀两个减号，组合要求：字母、数字、减号
+#                           =''         定义参数或选项的默认值，选项指定后为带值选项，默认值为空时可省略后面的引号
+#                                       默认值允许使用：字母、数字、减号、下划线、转义组合，或者使用单双引号
+#                                       转义符在无引号或有引号时均有效使用时需要注意
+#                           {validate}  选项或参数验证，验证支持参考函数：validate_shell_param
 # @param $options_name      命令参数数组名
 # return 1|0
-parse_command_param() {
-    local PARAM NAME SHORT_NAME DEFAULT_VALUE DEFAULT_VALUE_STR PARAM_STR PARAM_INFO_STR PARAM_NAME_STR PARAM_SHOW_DEFINE PARAM_SHOW_INFO ARG_NAME INDEX SPACE_NUM=22 ARGUMENTS=() OPTIONS=() OPTIONALS=() ARGVS=() COMMENT_SHOW_ARGUMENTS='' COMMENT_SHOW_OPTIONS=''
+parse_shell_param() {
+    local PARAM NAME SHORT_NAME DEFAULT_VALUE DEFAULT_VALUE_STR DEFAULT_VALIDATE_STR PARAM_STR PARAM_INFO_STR PARAM_NAME_STR PARAM_SHOW_DEFINE PARAM_SHOW_INFO ARG_NAME INDEX 
+    local SPACE_NUM=22 ARGUMENTS=() OPTIONS=() OPTIONALS=() ARGVS=() VALIDATES_NAME_QUEUE=() VALIDATES_RULE_QUEUE=() COMMENT_SHOW_ARGUMENTS='' COMMENT_SHOW_OPTIONS=''
+    local REGEXP_ARGV='(-[a-z0-9]\s*,\s*)?--[a-z0-9][a-z0-9\-]+|[a-z0-9][a-z0-9\-]+' REGEXP_VALIDATE="\{\s*([,:\|]+|$REGEXP_QUOTE_STRING)*\s*\}"
     # 解析定义的参数
     while read -r PARAM; do
         if [ -z "$PARAM" ] || printf '%s' "$PARAM"|grep -qP '^\s*$'; then
@@ -372,14 +392,21 @@ parse_command_param() {
             fi
             continue;
         fi
-        PARAM_STR=$(printf '%s' "$PARAM"|grep -oiP "^\s*\[\s*((-[a-z0-9]\s*,\s*)?--[a-z0-9][\w\-]+|[a-z0-9][\w\-]+)(\s*=\s*(\w+|\"([^\"]|\\\\.)*\"|'([^']|\\\\.)*'))?\s*\]\s*")
+        # 提取匹配的定义参数语句结构
+        PARAM_STR=$(printf '%s' "$PARAM"|grep -oiP "^\s*\[\s*($REGEXP_ARGV)(\s*=\s*($REGEXP_QUOTE_STRING)?)?(\s*,\s*$REGEXP_VALIDATE)?\s*\]")
         if [ -z "$PARAM_STR" ];then
             error_exit "定义参数解析失败：$PARAM"
         fi
+        # 去掉首尾多余字符
         PARAM_INFO_STR=`printf '%s' "$PARAM_STR"|sed -r "s/(^\s*\[\s*)|(\s*\]\s*$)//g"`
-        PARAM_NAME_STR=`printf '%s' "$PARAM_INFO_STR"|grep -oP '^[^=]+'`
-        DEFAULT_VALUE_STR=$(printf '%s' "$PARAM_INFO_STR"|grep -oP "=\s*(\w+|\"([^\"]|\\\\.)*\"|'([^']|\\\\.)*')$")
-        if printf '%s' "$PARAM_NAME_STR"|grep -qP '^-{1,2}';then
+        # 提取参数名定义
+        PARAM_NAME_STR=`printf '%s' "$PARAM_INFO_STR"|grep -oP "^($REGEXP_ARGV)"`
+        # 提取参数默认值定义
+        DEFAULT_VALUE_STR=$(printf '%s' "${PARAM_INFO_STR:${#PARAM_NAME_STR}}"|grep -oP "^\s*=\s*($REGEXP_QUOTE_STRING)?")
+        # 提取参数验证规则
+        DEFAULT_VALIDATE_STR=$(printf '%s' "${PARAM_INFO_STR}"|grep -oP "$REGEXP_VALIDATE$"|sed -r "s/(^\s*\{\s*)|(\s*\}\s*$)//g")
+        # 参数类型识别处理
+        if [[ "$PARAM_NAME_STR" =~ ^-{1,2} ]];then
             SHORT_NAME=`printf '%s' "$PARAM_NAME_STR"|grep -oiP '^-[a-z0-9]'`
             NAME=`printf '%s' "$PARAM_NAME_STR"|grep -oiP '\-\-[a-z0-9][\w\-]+'`
             PARAM_SHOW_DEFINE="$NAME"
@@ -398,6 +425,7 @@ parse_command_param() {
             PARAM_SHOW_DEFINE="$NAME"
             ARG_NAME='ARGU_'
         fi
+        # 重复参数校验
         for ((INDEX=0; INDEX < ${#ARGVS[@]}; INDEX++)); do
             if test ${ARGVS[$INDEX]} = $NAME || test ${ARGVS[$INDEX]} = "$SHORT_NAME";then
                 error_exit "不能定义重名参数: ${ARGVS[$INDEX]} , $PARAM"
@@ -407,21 +435,27 @@ parse_command_param() {
         if [ -n "$SHORT_NAME" ];then
             ARGVS[${#ARGVS[@]}]="$SHORT_NAME"
         fi
+        # 参数默认值写入参数可访问全局变量中
         ARG_NAME=$ARG_NAME`printf '%s' "$NAME"|sed -r "s/^-{1,2}//"|sed "s/-/_/g"`
         if [ -n "$DEFAULT_VALUE_STR" ];then
             DEFAULT_VALUE_STR=$(printf '%s' "$DEFAULT_VALUE_STR"|sed -r "s/(^=\s*)//")
-            PARAM_SHOW_DEFINE="$PARAM_SHOW_DEFINE [= $DEFAULT_VALUE_STR]"
-            DEFAULT_VALUE=$(printf '%s' "$DEFAULT_VALUE_STR"|sed -r "s/(^['\"])|(['\"]$)//g"|sed -r "s/\\\\(.)/\1/g")
-            eval "$ARG_NAME=\$DEFAULT_VALUE"
+            PARAM_SHOW_DEFINE="$PARAM_SHOW_DEFINE [= ${DEFAULT_VALUE_STR:-''}]"
+            get_escape_string "$DEFAULT_VALUE_STR" $ARG_NAME
         elif ! declare -p $ARG_NAME 2>/dev/null >/dev/null;then
             eval "$ARG_NAME=''"
         fi
+        # 参数有验证写入待验证队列中
+        if [ -n "$DEFAULT_VALIDATE_STR" ];then
+            VALIDATES_NAME_QUEUE[${#VALIDATES_NAME_QUEUE[@]}]="$ARG_NAME"
+            VALIDATES_RULE_QUEUE[${#VALIDATES_RULE_QUEUE[@]}]="$DEFAULT_VALIDATE_STR"
+        fi
+        # 构造帮助命令中参数展示
         INDEX=`echo $[ $(printf '%s' "$PARAM_SHOW_DEFINE"|wc -m) + 4 ]`
         PARAM_SHOW_INFO="    $PARAM_SHOW_DEFINE"
         if ((INDEX >= SPACE_NUM));then
             PARAM_SHOW_INFO="$PARAM_SHOW_INFO\n"
         fi
-        PARAM_SHOW_INFO=$PARAM_SHOW_INFO$(printf '%*s' $((INDEX >= SPACE_NUM ? SPACE_NUM : SPACE_NUM-INDEX)))$(printf '%s' "$PARAM"|sed -r "s/^.{1,`echo $(printf '%s' "$PARAM_STR"|wc -m)`}//")
+        PARAM_SHOW_INFO=$PARAM_SHOW_INFO$(printf '%*s' $((INDEX >= SPACE_NUM ? SPACE_NUM : SPACE_NUM-INDEX)))${PARAM:${#PARAM_STR}}
         if [[ "$NAME" == -* ]];then
             COMMENT_SHOW_OPTIONS="$COMMENT_SHOW_OPTIONS$PARAM_SHOW_INFO \n"
         else
@@ -441,9 +475,9 @@ EOF
             continue
         fi
         NAME=''
-        if printf '%s' "$ITEM"|grep -qiP '^((--[a-z0-9][\w\-]+(=.*)?)|(-[a-z0-9]))$'; then
+        if [[ "$ITEM" =~ ^(--[A-Za-z0-9][A-Za-z0-9\-]+=.*|-[A-Za-z0-9])$ ]]; then
             # 有参数的选项处理
-            if printf '%s' "$ITEM"|grep -qiP '^--[a-z0-9][\w\-]+=.*';then
+            if [[ "$ITEM" =~ ^--[A-Za-z0-9][A-Za-z0-9\-]+=.*$ ]];then
                 NAME_TEMP=$(printf '%s' "$ITEM"|grep -oiP '^--[a-z0-9][\w\-]+')
                 VALUE=$(printf '%s' "$ITEM"|sed -r "s/^[^=]+=//")
             else
@@ -452,7 +486,7 @@ EOF
                 for ((INDEX=0; INDEX < ${#OPTIONALS[@]}; INDEX++)); do
                     OPTIONS_TEMP=${OPTIONALS[$INDEX]}
                     if [ "$OPTIONS_TEMP" != "`printf '%s' "$OPTIONS_TEMP"|sed -r "s/$NAME_TEMP($|,)//"`" ];then
-                        NAME=$(printf '%s' "$OPTIONS_TEMP"|sed -r "s/(-[A-Za-z0-9]\s*,\s*)?--//")
+                        NAME=${OPTIONS_TEMP#*--}
                         VALUE='1'
                         break
                     fi
@@ -462,8 +496,8 @@ EOF
                 for ((INDEX=0; INDEX < ${#OPTIONS[@]}; INDEX++)); do
                     OPTIONS_TEMP=${OPTIONS[$INDEX]}
                     if [ "$OPTIONS_TEMP" != "`printf '%s' "$OPTIONS_TEMP"|sed -r "s/$NAME_TEMP($|,)//"`" ];then
-                        NAME=$(printf '%s' "$OPTIONS_TEMP"|sed -r "s/(-[A-Za-z0-9]\s*,\s*)?--//")
-                        if [ -z "$VALUE" ] && printf '%s' "$NAME_TEMP"|grep -qiP '^-[a-z0-9]$';then
+                        NAME=${OPTIONS_TEMP#*--}
+                        if [[ -z "$VALUE" && "$NAME_TEMP" =~ ^-[a-z0-9]$ ]];then
                             ((ARG_NUM++))
                             eval "VALUE=\${$2[$ARG_NUM]}"
                         fi
@@ -485,10 +519,11 @@ EOF
         if [ -z "$NAME" ];then
             warn_msg "未知参数: "$ITEM
         else
-            eval "$ARG_NAME`printf '%s' "$NAME"|sed -r "s/^-{1,2}//"|sed "s/-/_/g"`=\$VALUE"
+            eval "$ARG_NAME${NAME//-/_}=\$VALUE"
         fi
     done
     if [ "$ARGV_help" = '1' ];then
+        # 帮助命令强制进入并终止后续执行
         local PARAMS_NAME=() INFO_SHOW_STR=""
         if [ -n "$COMMENT_SHOW_ARGUMENTS" ];then
             PARAMS_NAME[${#PARAMS_NAME[@]}]='[Arguments]'
@@ -514,12 +549,182 @@ $RUN_SHOW_STR
 $INFO_SHOW_STR";
         exit
     elif [ "$ARGV_version" = '1' ];then
+        # 展示脚本版本号信息强制终止后续执行
         echo -e "linux服务器常用工具和安装shell脚本，当前版本号：$SHELL_RUN_VERSION
 脚本存放在github上，地址：https://github.com/ttlxihuan/shell"
     else
+        # 验证参数
+        for ((INDEX=0; INDEX < ${#VALIDATES_NAME_QUEUE[@]}; INDEX++)); do
+            validate_shell_param ${VALIDATES_NAME_QUEUE[$INDEX]} "${VALIDATES_RULE_QUEUE[$INDEX]}"
+        done
         return 0
     fi
     exit 0
+}
+# 验证脚本参数
+# @command validate_shell_param $name $rules
+# @param $name              参数变量全名
+# @param $rules             参数规则要求，多个使用|分开
+#                           支持规则有：
+#                               required                                必填项
+#                               required_with:name[,name...]            指定变量有存在时必填，name是参数变量名
+#                               required_with_all:name[,name...]        指定变量全部存在时必填，name是参数变量名
+#                               required_without:name[,name...]         指定变量有不存在时必填，name是参数变量名
+#                               required_without_all:name[,name...]     指定变量全部不存在时必填，name是参数变量名
+#                               required_if:name,value[,name,value...]  指定变量有等于指定值时必填，name是参数变量名，value是参数变量值
+#                               required_nif:name,value[,name,value...] 指定变量有不等于指定值时必填，name是参数变量名，value是参数变量值
+#                               int[:[min],[max]]       必需是整数，可以指定数值大小范围
+#                               float[:[min],[max]]     必需是浮点数，可以指定数值大小范围
+#                               string[:[min],[max]]    必需是字符串，可以指定长度范围
+#                               ip[:4|6]           必需是ip4或ip6地址，可以指定限定ip4或ip6
+#                               url                必需是有效URL
+#                               path               必需是存在的目录
+#                               file               必需是存在的文件
+#                               in:opt1,opt2,...   限制可选值
+#                               regexp:expr        正则表达式验证，正则使用 [[]] 命令处理
+#                           规则名要求：必需是支持的规则名
+#                           规则值要求：可以是单或双引号或无引号（特殊字符需要使用转义字符，非特殊字符有：字母、数字、下划线、减号）
+# return 1|0
+validate_shell_param(){
+    local RULE_TEXT RULE_NAME RULE_VALUES ARG_NAME ARG_VALUE LISTS_STR INDEX ARG_REQUIRED_OPT ARG_REQUIRED_VAL VALUE_RULES="($REGEXP_QUOTE_STRING)"
+    get_shell_option "$1" ARG_NAME ARG_VALUE
+    while read RULE_TEXT; do
+        RULE_NAME=$(printf '%s' "$RULE_TEXT"|grep -oP '^[^:\|]+')
+        if ! [[ "$RULE_NAME" =~ ^(required(_(n?if|(with|without)(_all)?))?|int|float|string|path|file|ip|url|in|regexp)$ ]];then
+            warn_msg "脚本参数 ${ARG_NAME} 未知验证：$RULE_NAME"
+            continue
+        fi
+        ([[ -z "$ARG_VALUE" && "$RULE_NAME" != required* ]] || [[ -n "$ARG_VALUE" && "$RULE_NAME" == required* ]]) && continue
+        RULE_VALUES=($(printf '%s' "${RULE_TEXT:${#RULE_NAME}+1}"|grep -oP "$VALUE_RULES"))
+        # 剥离值前后引号
+        for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX+=2));do
+            get_escape_string "${RULE_VALUES[$INDEX]}" RULE_VALUES[$INDEX]
+        done
+        [[ ! "$RULE_NAME" =~ ^(int|float|string)$ || "${RULE_VALUES[0]}" =~ ^([1-9][0-9]*|[0-9])*$ && "${RULE_VALUES[2]}" =~ ^([1-9][0-9]*|[0-9])*$ ]] ||
+            error_exit "脚本参数 ${ARG_NAME} 校验规则错误，选项必需是数值范围：${RULE_TEXT}"
+        LISTS_STR=''
+        case "$RULE_NAME" in
+            int)
+                [[ "$ARG_VALUE" =~ ^[\-\+]?([1-9][0-9]*|[0-9])$ ]] || error_exit "脚本参数 ${ARG_NAME} 必需是整数，当前是：$ARG_VALUE"
+                [ -z "${RULE_VALUES[0]}" ] || (( ARG_VALUE >= ${RULE_VALUES[0]} )) || error_exit "脚本参数 ${ARG_NAME} 必需 >= ${RULE_VALUES[0]}，当前是：$ARG_VALUE"
+                [ -z "${RULE_VALUES[1]}" ] || (( ARG_VALUE <= ${RULE_VALUES[1]} )) || error_exit "脚本参数 ${ARG_NAME} 必需 <= ${RULE_VALUES[1]}，当前是：$ARG_VALUE"
+            ;;
+            float)
+                tools_install bc
+                [[ "$ARG_VALUE" =~ ^-?([1-9][0-9]*|[0-9])(\.[0-9]+)?$ ]] || error_exit "脚本参数 ${ARG_NAME} 必需是浮点数，当前是：$ARG_VALUE"
+                [ -z "${RULE_VALUES[0]}" -o $(echo "$ARG_VALUE >= ${RULE_VALUES[0]}"|bc) = '1' ] || error_exit "脚本参数 ${ARG_NAME} 必需 >= ${RULE_VALUES[0]}，当前是：$ARG_VALUE"
+                [ -z "${RULE_VALUES[0]}" -o $(echo "$ARG_VALUE <= ${RULE_VALUES[1]}"|bc) = '1' ] || error_exit "脚本参数 ${ARG_NAME} 必需 <= ${RULE_VALUES[1]}，当前是：$ARG_VALUE"
+            ;;
+            string)
+                [ -z "${RULE_VALUES[0]}" ] || (( ${#ARG_VALUE} >= ${RULE_VALUES[0]} )) || error_exit "脚本参数 ${ARG_NAME} 字符长度必需 >= ${RULE_VALUES[0]}，当前是：$ARG_VALUE"
+                [ -z "${RULE_VALUES[1]}" ] || (( ${#ARG_VALUE} <= ${RULE_VALUES[1]} )) || error_exit "脚本参数 ${ARG_NAME} 字符长度必需 <= ${RULE_VALUES[1]}，当前是：$ARG_VALUE"
+            ;;
+            path)
+                [[ "$ARG_VALUE" =~ ^[~/] && -d "$ARG_VALUE" ]] || [[ "$ARG_VALUE" =~ ^[^~/] && -d "$SHELL_WROK_BASH_PATH/$ARG_VALUE" ]] ||
+                error_exit "脚本参数 ${ARG_NAME} 指定目录不存在，当前是：$ARG_VALUE"
+            ;;
+            file)
+                [[ "$ARG_VALUE" =~ ^[~/] && -e "$ARG_VALUE" ]] || [[ "$ARG_VALUE" =~ ^[^~/] && -e "$SHELL_WROK_BASH_PATH/$ARG_VALUE" ]] ||
+                error_exit "脚本参数 ${ARG_NAME} 指定文件不存在，当前是：$ARG_VALUE"
+            ;;
+            required)
+                error_exit "脚本参数 ${ARG_NAME} 不可为空"
+            ;;
+            required_if)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX+=2));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ -z "$ARG_REQUIRED_VAL" -o "$ARG_REQUIRED_VAL" != "${RULE_VALUES[$INDEX+1]}" ] || error_exit "脚本参数 ${ARG_NAME} 在 ${ARG_REQUIRED_OPT} = ${RULE_VALUES[$INDEX+1]} 时不可为空"
+                done
+            ;;
+            required_nif)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX+=2));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ "$ARG_REQUIRED_VAL" = "${RULE_VALUES[$INDEX+1]}" ] || error_exit "脚本参数 ${ARG_NAME} 在 ${ARG_REQUIRED_OPT} != ${RULE_VALUES[$INDEX+1]} 时不可为空"
+                done
+            ;;
+            required_with)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX++));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ -z "$ARG_REQUIRED_VAL" ] || error_exit "脚本参数 ${ARG_NAME} 在 ${ARG_REQUIRED_OPT} 指定后不可为空"
+                done
+            ;;
+            required_with_all)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX++));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ -z "$ARG_REQUIRED_VAL" ] && continue 2
+                    LISTS_STR="$LISTS_STR, $ARG_REQUIRED_OPT"
+                done
+                error_exit "脚本参数 ${ARG_NAME} 在 ${LISTS_STR:2} 指定后不可为空"
+            ;;
+            required_without)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX++));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ -n "$ARG_REQUIRED_VAL" ] || error_exit "脚本参数 ${ARG_NAME} 在 ${ARG_REQUIRED_OPT} 不指定时不可为空"
+                done
+            ;;
+            required_without_all)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX++));do
+                    get_shell_option "${RULE_VALUES[$INDEX]}" ARG_REQUIRED_OPT ARG_REQUIRED_VAL
+                    [ -n "$ARG_REQUIRED_VAL" ] && continue 2
+                    LISTS_STR="$LISTS_STR, $ARG_REQUIRED_OPT"
+                done
+                error_exit "脚本参数 ${ARG_NAME} 在 ${LISTS_STR:2} 不指定时不可为空"
+            ;;
+            ip)
+                [[ -z "${RULE_VALUES[0]}" || "${RULE_VALUES[0]}" =~ ^[46]$ ]] || error_exit "脚本参数 ${ARG_NAME} 校验规则错误，选项必需4或6：${RULE_TEXT}"
+                [[ "${RULE_VALUES[0]}" =~ ^4?$ && "$ARG_VALUE" =~ ^[0-9]{1,3}(\.[0-9]{1,3}){3}$ ]] || 
+                [[ "${RULE_VALUES[0]}" =~ ^6?$ && "$ARG_VALUE" =~ ^[0-9A-Fa-f]{1,4}(:[0-9A-Fa-f]{1,4}){7}$ ]] ||
+                error_exit "脚本参数 ${ARG_NAME} 不是有效ip${RULE_VALUES[0]:-4/6}地址，当前是：$ARG_VALUE"
+            ;;
+            url)
+                tools_install curl
+                curl -I -m 5 "$ARG_VALUE" 2>/dev/null >/dev/null || error_exit "脚本参数 ${ARG_NAME} 不是有效url地址，当前是：$ARG_VALUE"
+            ;;
+            in)
+                for((INDEX=0;INDEX<${#RULE_VALUES[@]};INDEX++));do
+                    if [ "${RULE_VALUES[$INDEX]}" = "$ARG_VALUE" ];then
+                        ARG_EXIST=1
+                        continue 2
+                    fi
+                    LISTS_STR="$LISTS_STR, ${RULE_VALUES[$INDEX]}"
+                done
+                error_exit "脚本参数 ${ARG_NAME} 必需是可选值其一：${LISTS_STR:2}，当前是：$ARG_VALUE"
+            ;;
+            regexp)
+                [ -z "${RULE_VALUES[0]}" ] || eval [[ "\${ARG_VALUE}" =~ ${RULE_VALUES[0]} ]] || error_exit "脚本参数 ${ARG_NAME} 不符合正则表达式：${RULE_VALUES[0]}，当前是：$ARG_VALUE"
+            ;;
+            *)
+                warn_msg "脚本参数 ${ARG_NAME} 无处理验证：$RULE_NAME"
+            ;;
+        esac
+    done <<EOF
+$(printf '%s' "$2"|grep -oP "[^:\|]+(:$VALUE_RULES(,$VALUE_RULES)*)?\|?")
+EOF
+}
+# 获取转义后的字符串
+# @command get_escape_string $string $export_name
+# @param $string            要处理的字符串，前后引号会玻璃，内部转义符会全部替换
+# @param $export_name       解析成功写入变量名
+# return 1|0
+get_escape_string(){
+    local _TEMP_STRING=$(printf '%s' "$1"|sed -r "s/(^['\"])|(['\"]$)//g"|sed -r "s/\\\\(.)/\1/g")
+    eval $2=\$_TEMP_STRING
+}
+# 通过变量名获取shell脚本选项信息
+# @command get_shell_option $name $set_opt $set_val
+# @param $name              要解析变量名
+# @param $set_opt           解析选项成功写入变量名
+# @param $set_val           解析选项值成功写入变量名
+# return 1|0
+get_shell_option(){
+    local SHELL_OPTION_PREFIX='' SHELL_OPTION_NAME="${1:5}"
+    if [[ "$1" == ARGV_* ]];then
+        SHELL_OPTION_PREFIX='--'
+    elif [[ "$1" != ARGU_* ]];then
+        error_exit "${1} 未知校验参数"
+    fi
+    eval $2="$SHELL_OPTION_PREFIX${SHELL_OPTION_NAME//_/-}"
+    eval $3=\${$1}
 }
 # 解析列表并去重再导出数组
 # @command parse_lists $export_name $string $separator $match
@@ -596,6 +801,7 @@ SHELL_WROK_INSTALLS_PATH=${SHELL_WROK_BASH_PATH}/installs
 SHELL_WROK_TOOLS_PATH=${SHELL_WROK_BASH_PATH}/tools
 SHELL_WROK_TEMP_PATH=${SHELL_WROK_BASH_PATH}/temp
 SHELL_WROK_ETC_PATH=${SHELL_WROK_BASH_PATH}/etc
+REGEXP_QUOTE_STRING="([\w\-]+|\\\\.)+|\"([^\"]+|\\\\.)*\"|'([^']+|\\\\.)*'"
 # 加载配置
 source $SHELL_WROK_INCLUDES_PATH/config.sh || exit
 # 提取安装参数
