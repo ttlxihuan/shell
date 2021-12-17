e878147f5a3bba50a31cffa193128e5e779cf3fb
huan.xi <huan.xi@broadshare.cn>
2021-12-17 20:14:52 +0800

    批量安装基本处理，待完善

--- installs/install-batch.sh
+++ installs/install-batch.sh
@@ -7,54 +7,82 @@ if [ $(basename "$0") = $(basename "${BASH_SOURCE[0]}") ];then
     SHELL_RUN_DESCRIPTION='批量安装'
 fi
 SHELL_RUN_HELP='
-批量安装仅限支持的脚本，多个安装并行操作
-批量安装会忽略磁盘空间，自动适配虚拟内存进行安装。
+1、批量安装仅限支持的脚本，多个安装并行操作
+2、批量安装默认会忽略磁盘空间（建议磁盘剩余空间 >= 50G）
+3、批量安装默认会自动适配虚拟内存，当物理内存不足时自动添加虚拟内存
 '$SHELL_RUN_HELP
-DEFINE_TOOL_PARAMS="$DEFINE_TOOL_PARAMS
-[-f, --config=':etc/install-batch.conf'] 指定配置文件
+DEFINE_TOOL_PARAMS="
+[name, {required}]要安装的包或组名，多个使用逗号分开
+#包或组名必需在配置文件中指定
+[-f, --conf='etc/install-batch.conf', {required|file}]指定配置文件，相对脚本库根目录
+$DEFINE_TOOL_PARAMS
 "
 source $(cd $(dirname ${BASH_SOURCE[0]}); pwd)/../includes/tool.sh || exit
-# 提取配置文件路径
-if ! get_file_path $ARGV_config ARGV_config 1;then
-    error_exit "--config 未指定有效配置文件：$ARGV_config"
-fi
-# 安装服务包
-# @command install_server $name [$options ...]
-# @param $name              指定安装服务名，比如：php
-# @param $options           指定安装版脚本定制参数集
+# 运行安装操作
+# @command run_install $name
+# @param $name              要安装的包或组名，多个使用逗号分开
 # return 1|0
-install_server(){
-    local INSTALL_FILE_PATH
-    find_project_file install "$1" INSTALL_FILE_PATH
-    if ps aux|grep -P "$1-install\.sh\s+(new|\d+\.\d+)"; then
-        warn_msg "$1 已经在安装运行中"
-    else
-        info_msg "安装：$1 ，安装信息保存在：$SHELL_WROK_TEMP_PATH/$1-install.log"
-        run_msg "nohup bash $INSTALL_FILE_PATH ${@:2} --disk-space=ignore --memory-space=swap >> $SHELL_WROK_TEMP_PATH/$1-install.log 2>/dev/null &"
+run_install(){
+    if (( (RUN_INSTALL_RECURSION++) > 10 ));then
+        error_exit "$1 递归安装已经超过10层，请确认配置是否异常"
+    fi
+    local NAME INSTALL_INFO LOG_FILE
+    # 循环提取要安装的包名
+    while read NAME ;do
+        if [[ "$NAME" == @* ]];then
+            get_conf INSTALL_INFO install-group "${NAME#*@}"
+            run_install "$INSTALL_INFO"
+        else
+            get_conf INSTALL_INFO install "$NAME"
+            if [ -z "$INSTALL_INFO" ];then
+                INSTALL_INFO="$NAME"
+            fi
+            if ! [[ "${INSTALL_INFO#* }" =~ [[:space:]]+[^[:space:]]+ ]];then
+                INSTALL_INFO="$INSTALL_INFO new"
+            fi
+            NAME=${INSTALL_INFO%% *}
+            find_project_file install "${NAME}" INSTALL_FILE_PATH
+            if has_run_shell "${NAME}-install"; then
+                warn_msg "${NAME} 已经在安装运行中"
+            else
+                install_log_file LOG_FILE "${NAME}"
+                info_msg "安装 ${NAME} 标准输出信息保存在：$LOG_FILE"
+                run_msg "bash $INSTALL_FILE_PATH ${INSTALL_INFO#* } --disk-space=ignore --memory-space=swap >$LOG_FILE 2>&1" && echo '安装 ${NAME} 成功' &
+            fi
+        fi
+    done <<EOF
+$(printf '%s' "$1"|grep -oP '[^,\s]+')
+EOF
+}
+# 获取安装日志文件
+# @command install_log_file $set_value $name
+# @param $set_value         要写入变量名
+# @param $name              安装信息名
+# return 1|0
+install_log_file(){
+    eval $1="$SHELL_WROK_TEMP_PATH/${2}-install-$(date +'%Y-%m-%d').log"
+    echo '[安装时间]'$(date +'%Y-%m-%d %H:%M:%S') >> $1
+}
+# 开始批量安装操作
+# @command start_install $wait
+# @param $wait              是否等待安装完成
+#                           任何非空字符均表示等待批量安装完成
+# return 1|0
+start_install(){
+    info_msg "开始本地批量安装"
+    # 执行安装
+    run_install "$ARGU_name"
+    info_msg "所有安装包命令已经在后台并行执行中"
+    if [ -n "$1" ];then
+        info_msg "等待本地所有批量安装子进程结束"
+        wait
     fi
 }
-# 读取配置文件
-# @command read_config $node_name $command_name
-# @param $node_name         指定配置节点，配置文件中的[node_name]块内容
-# @param $command_name      读取配置后调用的命令
-# return 1|0
-read_config(){
-    cat $ARGV_config | grep "\[$1\]" -A $CONFIG_FILE_LINE -m 1 | sed -n '2,$p' | while read LINE
-    do
-        # 下一个配置块就退出
-        if [ -n "`echo $LINE|grep -P "^\[[\w-]+\].*"`" ]; then
-            break;
-        fi
-        # 注释或空行就进入下一次循环
-        if echo "$LINE"|grep -qP "^((\s*#.*)|([\t\n\s\r]*))$" || [ -z "$LINE" ]; then
-            continue;
-        fi
-        $2 $LINE
-    done
-}
-# 获取文件长度
-CONFIG_FILE_LINE=`wc -l $ARGV_config|grep -P '\d+' -o`
+# 安装递归调用层次
+RUN_INSTALL_RECURSION=0
+# 配置文件解析
+parse_conf $ARGV_conf install install-group remote $CONF_BLOCKS
+# 如果是当前脚本为入口文件则为本地批量安装
 if [ $(basename "$0") = $(basename "${BASH_SOURCE[0]}") ];then
-    read_config install install_server
+    start_install 1
 fi
-
--- installs/install-remote.sh
+++ installs/install-remote.sh
@@ -5,104 +5,207 @@
 # 参数信息配置
 SHELL_RUN_DESCRIPTION='远程批量安装'
 DEFINE_TOOL_PARAMS="
-[-d, --remote-dir='~/shell/'] 远程执行目录，shell脚本会复制到这个目录下并进行安装
+[remote-match]匹配要安装的远程服务器配置名
+#允许使用正则表达式匹配
+#匹配的并非远程服务器地址而是节点配置名
+[-d, --remote-dir='~/shell-script/', {required}]远程服务器shell脚本保存目录
+#shell脚本会复制到这个目录下并进行安装
 #在进行安装前需要保证目录可用空间，空间不足会造成安装失败
-[-s, --skip-local] 跳过本地安装，默认本地先安装，安装完后再安装远程，避免并行安装复制缺失文件
-[-u, --uncopy-temp] 不复制安装目录，所以远程服务重新下载安装
-[--disabled-expect] 禁止使用expect工具自动输入密码
-#禁止后需要手动输入密码多次方可进行安装
-[--disabled-sshkey] 禁止使用证书登录，证书登录可以减少密码输入次数
+[-i, --local-install='async', {required|in:async,before,skip}]本地批量安装处理
+#async  异步安装远程与本地并行
+#       各服务不复制安装文件
+#       各服务器均需要自行下载安装包
+#before 先安装本地并在安装完后再安装远程
+#       安装远程时会复制安装文件
+#       各服务不需要再下载相同的文件
+#skip   不执行本地安装，仅执行远程安装
+#       将复制安装文件致远程服务器
+#       此选项不可与本地批量安装并行
+#       当本地有并行安装时将终止批量安装
+#执行远程批量安装时尽量避免本地另执行批量安装
+#本地交叉批量安装会影响复制文件完整导致远程安装异常
+[--disable-expect]禁止使用expect工具自动输入密码
+#禁用后配置文件中的密码将无法自动输入并登录
+[--ssh-key='~/.ssh/id_rsa.pub']指定本地登录远程服务器证书地址
+#证书地址文件不存在时会自动创建
+#为空时将处理证书登录和创建操作
+#为空时如果已经配置好证书登录将自动使用
 "
 SHELL_RUN_HELP='
-3、远程安装依赖三个工具 ssh scp ，脚本会自动安装。
-4、远程安装流程：证书处理、创建目录、复制脚本、执行安装。每个环节均有可能输入密码
-5、证书不建议设置密码，否则证书使用意义不大
-6、使用远程安装时不可另外执行本地批量安装并行操作，会影响远程安装复制文件导致安装失败
+
+远程安装要求：
+1、远程安装依赖三个工具 ssh scp ，脚本会自动安装。
+2、远程安装流程：证书处理、创建目录、复制脚本、执行安装。每个环节均有可能输入密码
+3、使用的证书不建议设置密码，否则证书登录时还需要输入密码
+4、使用远程安装时不可另外执行本地批量安装并行操作，会影响远程安装复制文件导致安装失败
 '
 source $(cd $(dirname ${BASH_SOURCE[0]}); pwd)/install-batch.sh || exit
-if [ -z "$ARGV_skip_local" ];then
-    info_msg '本机批量安装'
-    read_config install install_server
-fi
+# 开始远程安装服务
+# @command start_ssh_install $item_name $item_value
+# @param $item_name         配置区块内项名，远程节点名
+# @param $item_value        配置区块内项值，远程节点信息
+# return 1|0
+start_ssh_install(){
+    # 解析远程服务器配置信息
+    local REMOTE_INFO=($2)
+    if [ "${#REMOTE_INFO[@]}" = '0' ];then
+        warn_msg "$1 远程配置节点信息为空，跳过安装"
+        return 1
+    fi
+    info_msg "${REMOTE_INFO[0]} 远程服务器安装"
+    local SSH_USER=root SSH_HOST="${REMOTE_INFO[0]}" SSH_PORT=22 SSH_PASSWORD="${REMOTE_INFO[1]}" SSH_ROOT_PASSWORD="${REMOTE_INFO[2]}"
+    if [[ -z "$ARGV_disable_expect" && "$SSH_PASSWORD$SSH_ROOT_PASSWORD" =~ '"' ]];then
+        warn_msg "$1 远程配置节点使用 expect 密码不能包含双引号，跳过安装"
+        return 1
+    fi
+    # 获取指定登录用户名
+    if [[ "${REMOTE_INFO[0]}" =~ .+@.+ ]];then
+        SSH_USER="${REMOTE_INFO[0]%%@*}"
+        SSH_HOST="${SSH_HOST#*@}"
+    fi
+    if [[ -z "$SSH_USER" ]];then
+        warn_msg "$1 远程配置节点登录用户名为空，跳过安装"
+        return 1
+    fi
+    # 获取指定登录端口号
+    if [[ "${REMOTE_INFO[0]}" =~ .+:.+ ]];then
+        SSH_PORT="${REMOTE_INFO[0]##*:}"
+        SSH_HOST="${SSH_HOST%*:}"
+    fi
+    if [[ -z "$SSH_PORT" || ! "$SSH_PORT" =~ ^([0-9]|[1-9][0-9]{0,5})$ ]] || (( SSH_PORT >= 65536 || SSH_PORT == 0 ));then
+        warn_msg "$1 远程配置节点端口号错误：$SSH_PORT，跳过安装"
+        return 1
+    fi
+    if [[ -z "$SSH_HOST" ]];then
+        warn_msg "$1 远程配置节点登录地址为空，跳过安装"
+        return 1
+    fi
+    local LOG_FILE EXPECT_PASSWORD START_TIME=$(date +'%s') KEYGEN_LOGIN=1 SSH_ADDR="$SSH_USER@$SSH_HOST"
+    local SSH_CHECK="ssh -q -o ConnectTimeout=15 -o PasswordAuthentication=no -o StrictHostKeyChecking=no $SSH_KEY_OPTION -p $SSH_PORT $SSH_ADDR"
+    # 证书登录尝试
+    if ! $SSH_CHECK; then
+        if (( START_TIME + 14 < $(date +'%s') ));then
+            warn_msg "$1 远程配置节点连接超时（15秒）或网速过慢，跳过安装"
+            return 1
+        fi
+        warn_msg "$1 远程配置节点证书登录失败"
+        # 证书处理
+        if [ -n "$SSH_KEY_OPTION" ];then
+            info_msg "尝试复制 ssh-key 证书到 $1 服务器"
+            if [ -z "$ARGV_disable_expect" -a -n "$SSH_PASSWORD" ];then
+                info_msg "通过 expect 自动完成证书复制"
+                expect_password EXPECT_PASSWORD "$SSH_PASSWORD"
+                expect <<CMD
+spawn ssh-copy-id $SSH_KEY_OPTION "-p $SSH_PORT $SSH_ADDR"
+$EXPECT_PASSWORD
+CMD
+            else
+                run_msg ssh-copy-id $SSH_KEY_OPTION "-p $SSH_PORT $SSH_ADDR"
+            fi
+        else
+            info_msg "没有指定证书，跳过证书登录处理"
+        fi
+        if $SSH_CHECK; then
+            info_msg "证书复制成功"
+        else
+            warn_msg "证书复制失败"
+            KEYGEN_LOGIN=0
+        fi
+    fi
+    # 基本命令定义
+    local SCP_COMMAND="scp -r -f -P $SSH_PORT $COPY_REMOTE_FILE $SSH_ADDR:$ARGV_remote_dir"
+    if [ "$SSH_USER" = root ];then
+        local MKDIR_COMMAND="ssh $SSH_ADDR -C 'mkdir -p $ARGV_remote_dir'"
+        local BASH_COMMAND="ssh $SSH_ADDR -C 'cd $ARGV_remote_dir && tar -xzf $COPY_REMOTE_FILE && bash ./installs/install-batch.sh \"$ARGU_name\" &'"
+    else
+        local MKDIR_COMMAND="ssh $SSH_ADDR -C 'echo \"mkdir -p $ARGV_remote_dir && chown -R $SSH_USER:$SSH_USER $ARGV_remote_dir\"|sudo -u root -s'"
+        local BASH_COMMAND="ssh $SSH_ADDR -C 'cd $ARGV_remote_dir && tar -xzf $COPY_REMOTE_FILE && sudo -u root bash $ARGV_remote_dir/installs/install-batch.sh \"$ARGU_name\"'"
+    fi
+    install_log_file LOG_FILE "$1-${SSH_HOST}"
+    # 选项执行方式
+    if [ -n "$ARGV_disable_expect" ] || [ "$KEYGEN_LOGIN" = '1' -a "$SSH_USER" = root ] || [ "$SSH_USER" != root -a -z "$SSH_ROOT_PASSWORD" ];then
+        run_msg "$MKDIR_COMMAND && 
+$SCP_COMMAND && 
+$BASH_COMMAND > &"
+    else
+        info_msg "通过 expect 自动完成远程复制和安装"
+        local EXPECT_ROOT_PASSWORD
+        if [ "$KEYGEN_LOGIN" = '0' -a -z "$EXPECT_PASSWORD" ];then
+            expect_password EXPECT_PASSWORD "$SSH_PASSWORD"
+        fi
+        if [ "$SSH_USER" != root ];then
+            expect_password EXPECT_ROOT_PASSWORD "$SSH_ROOT_PASSWORD"
+        fi
+        expect & <<CMD
+spawn $MKDIR_COMMAND
+$EXPECT_PASSWORD
+$EXPECT_ROOT_PASSWORD
+spawn $SCP_COMMAND
+$EXPECT_PASSWORD
+spawn $BASH_COMMAND
+$EXPECT_PASSWORD
+$EXPECT_ROOT_PASSWORD
+CMD
+    fi
+}
+# 获取 expect 密码输入操作
+# @command expect_password $set_value $password
+# @param $set_value         写入变量名
+# @param $password          要输入的密码
+# return 1|0
+expect_password(){
+    eval "$1='expect {
+    \"*yes/no\" { send \"$2\r\"; exp_continue }
+    \"*password:\" { send \"$2\r\" }
+}'"
+}
 # 安装必需工具
 tools_install ssh scp
-if [ -z "$ARGV_disabled_expect" ];then
+if [ -z "$ARGV_disable_expect" ];then
     tools_install expect
 fi
 # 生成证书
-if [ -z "$ARGV_disabled_sshkey" -a ! -e ~/.ssh/id_rsa.pub ];then
-    info_msg '当前账号无证书，即将生成证书用于登录远程服务器'
-    ssh-keygen -t rsa <<EOF
-
-
-
-EOF
+if [ -n "$ARGV_ssh_key" ];then
+    SSH_KEY_DIR=$(dirname $ARGV_ssh_key)
+    if [ ! -d $SSH_KEY_DIR ];then
+        mkdirs $SSH_KEY_DIR
+        chmod 700 $SSH_KEY_DIR
+    fi
+    SSH_KEY_FILE=$(cd $SSH_KEY_DIR; pwd)/$(basename "$ARGV_ssh_key")
+    if [ ! -e "$SSH_KEY_FILE" ];then
+        info_msg "当前账号无证书，生成证书保存到：$SSH_KEY_FILE"
+        ssh-keygen -t rsa -f "$SSH_KEY_FILE" -P ''
+        if_error "证书生成失败"
+    fi
+    # 生成证书使用选项
+    SSH_KEY_OPTION="-i '$SSH_KEY_FILE'"
 fi
-if ! if_command ifconfig;then
-    packge_manager_run install net-tools
-fi
-# 获取证书路径
-SSH_KEY_PATH=`stat -c '%n' ~/.ssh/id_rsa.pub`
-# 远程安装服务
-# @command ssh_install_server $host [$password] [$user]
-# @param $host          要连接的服务器
-# @param $password      服务器登录密码，本地时不需要，如果服务器已经配置好证书登录也不需要传或传空
-# @param $user          服务器登录用户名，转为为root
-# return 1|0
-ssh_install_server(){
-    # 判断是否为本机
-    if [[ "$1" == 127.0.0.1|localhost ]] || ifconfig|grep -q "$1";then
-        warn_msg "远程地址是本地服务器：$1 ，跳过"
-        return 1
-    fi
-    info_msg "$1 远程服务器安装"
-    local SSH_USER=${3-root} KEYGEN_HAS=''
-    if ! ssh -o ConnectTimeout=30 -o PasswordAuthentication=no -o StrictHostKeyChecking=no -n $SSH_USER@$1 2>&1 >/dev/null; then
-        if [ -z "$2" ];then
-            warn_msg "服务器 $SSH_USER@$1 未配置密码，且无证书登录，无法进行远程操作安装"
-            return 1
+# 本地安装方式处理
+TAR_OPTIONS=''
+case "$ARGV_local_install" in
+    async)
+        start_install
+        TAR_OPTIONS='--exclude=./temp'
+    ;;
+    before)
+        start_install 1
+    ;;
+    skip)
+        if has_run_shell "*-install"; then
+            error_exit "检测到本地已经有安装脚本运行中"
         fi
-        if [ -z "$ARGV_disabled_sshkey" ];then
-            info_msg "复制 ssh-key 证书到 $SSH_USER@$1"
-            run_msg "ssh-copy-id -i $SSH_KEY_PATH $SSH_USER@$1"
-            if [ -z "$ARGV_disabled_expect" ];then
-                expect <<CMD
-set timeout 30
-spawn ssh-copy-id -i $SSH_KEY_PATH $SSH_USER@$1
-expect "*password*" {send "$2\r"; exp_continue}
-CMD
-            else
-                ssh-copy-id -i $SSH_KEY_PATH $SSH_USER@$1
-            fi
-            if [ $? != '0' ];then
-                warn_msg "证书复制失败"
-            else
-                KEYGEN_HAS=1
-            fi
-        fi
-    else
-        KEYGEN_HAS=1
-    fi
-    if [ "$KEYGEN_HAS" = '1' ] && ! ssh -o ConnectTimeout=30 -o PasswordAuthentication=no -n $SSH_USER@$1 2>&1 >/dev/null; then
-        warn_msg "ssh登录失败，请核对信息：$SSH_USER@$1"
-        return 1
-    fi
-    if [ -z "$ARGV_disabled_expect" -a "$KEYGEN_HAS" = '1' ];then
-        info_msg "即将通过 expect 调用以下命令"
-        expect <<CMD
-spawn ssh $SSH_USER@$1 -C 'mkdir -p $ARGV_remote_dir'
-expect "*password*" {send "$2\r"; exp_continue}
-spawn scp -r -f $SHELL_WROK_BASH_PATH $SSH_USER@$1:$ARGV_remote_dir
-expect "*password*" {send "$2\r"; exp_continue}
-spawn ssh $SSH_USER@$1 -C 'nohup bash $ARGV_remote_dir/installs/install-batch.sh &' &
-expect "*password*" {send "$2\r"; exp_continue}
-CMD
-    else
-        run_msg ssh $SSH_USER@$1 -C 'mkdir -p $ARGV_remote_dir'
-        run_msg scp -r $SHELL_WROK_BASH_PATH $SSH_USER@$1:$ARGV_remote_dir
-        run_msg ssh $SSH_USER@$1 -C 'nohup bash $ARGV_remote_dir/installs/install-batch.sh &' &
-    fi
-    return $?
-}
-# 开始安装
-read_config host ssh_install_server
+        info_msg '跳过本机批量安装'
+    ;;
+esac
+cd $SHELL_WROK_BASH_PATH
+COPY_REMOTE_FILE=./temp/remote-shell.tar.gz
+# 压缩脚本及相关文件
+info_msg "压缩脚本及相关文件到：$COPY_REMOTE_FILE"
+run_msg "tar -czf $COPY_REMOTE_FILE $TAR_OPTIONS ./"
+if_error "压缩文件失败，请确认权限和磁盘空间"
+# 循环远程配置数据并执行安装
+each_conf start_ssh_install remote
+run_msg "rm -f $COPY_REMOTE_FILE"
+# 最后还得等待所有子进程结束
+info_msg "等待所有批量安装子进程结束"
+wait
