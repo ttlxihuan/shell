447a82985b3b6382a6b86ed8ed8b5462eb5175ab
huan.xi <huan.xi@broadshare.cn>
2022-08-02 14:04:43 +0800

    服务管理优化

--- tools/services.sh
+++ tools/services.sh
@@ -30,8 +30,7 @@ DEFINE_TOOL_PARAMS='
 #不指定为全部配置服务处理
 [-f, --conf="etc/services.conf", {required|file}]指定配置文件，相对脚本根目录
 '
-
-source ${CURRENT_SHELL_BASH}includes/tool.sh || exit
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"/../includes/tool.sh || exit
 
 # 操作服务
 # @command handle_service $name
@@ -46,75 +45,109 @@ handle_service(){
     tag_msg "${HANDLE_INFO:-$1}"
     handle_run "$1" "$ARGU_action"
 }
+# 运行命令
+# @command run_command $name $command
+# @param $name          要操作的服务名
+# @param $action        要操作动作名
+# return 0|1
+run_command(){
+    local RUN_COMMAND RUN_USER
+    get_conf RUN_USER "$1" "user"
+    if get_conf RUN_COMMAND "$1" "$2"; then
+        if [ -n "$RUN_USER" ];then
+            sudo_msg "$RUN_USER" "$RUN_COMMAND"
+        else
+            run_msg "$RUN_COMMAND"
+        fi
+        if [ $? = '0' ];then
+            info_msg "$2 操作成功"
+        else
+            warn_msg "$2 操作失败"
+        fi
+    else
+        error_exit "$1 没有配置 ${2} 命令";
+    fi
+}
 # 操作处理
 # @command handle_run $name $action
 # @param $name          要操作的服务名
 # @param $action        要操作动作名
 # return 0|1
 handle_run(){
-    local RUN_COMMAND
     case "$2" in
         start)
             if handle_run "$1" status >/dev/null;then
                 warn_msg "服务已经启动"
                 return 1
             else
-                # 获取重动命令
-                if get_conf RUN_COMMAND "$1" "start-run"; then
-                    run_msg "$RUN_COMMAND"
-                else
-                    error_exit "没有配置启动命令";
-                fi
+                local LOOP_NUM
+                # 重复多次尝试启动服务
+                for ((LOOP_NUM=2;LOOP_NUM<5;LOOP_NUM++)); do
+                    # 获取重动命令
+                    run_command "$1" "start-run"
+                    sleep ${LOOP_NUM}s;
+                    if handle_run "$1" status >/dev/null;then
+                        break;
+                    else
+                        warn_msg "第${LOOP_NUM}次尝试启动 $1";
+                    fi
+                done
             fi
         ;;
         restart)
             # 获取重启命令
-            if get_conf RUN_COMMAND "$1" "restart-run"; then
-                run_msg "$RUN_COMMAND"
+            if has_conf "$1" "restart-run";then
+                run_command "$1" "restart-run"
             else
                 handle_run "$1" stop
                 handle_run "$1" start
             fi
         ;;
         stop)
-            if ! handle_run "$1" status >/dev/null;then
-                warn_msg "服务未启动"
-                return 1
-            fi
             # 获取停止命令
-            if get_conf RUN_COMMAND "$1" "stop-run"; then
-                run_msg "$RUN_COMMAND"
-            else
-                # 获取进程PID命令
-                local PID_NUM
-                get_handle_pid PID_NUM "$1"
-                run_msg kill $PID_NUM
-                info_msg "获取关闭结果："
-                local LOOP_NUM=30
-                # 循环获取关闭结果
-                while handle_run "$1" status >/dev/null;do
-                    sleep 1
-                    printf '.'
-                    if [ "$LOOP_NUM" == '10' ];then
-                        warn_msg "服务还在继续，尝试强制关闭"
-                        run_msg kill -9 $PID_NUM
-                        info_msg "获取关闭结果："
-                    elif [ "$LOOP_NUM" = '0' ];then
-                        error_exit "服务关闭失败"
+            if handle_run "$1" status >/dev/null;then
+                if has_conf "$1" "stop-run";then
+                    run_command "$1" "stop-run"
+                else
+                    # 获取进程PID命令
+                    local RUN_PID
+                    if get_handle_pid RUN_PID "$1";then
+                        run_msg kill $RUN_PID
+                        local LOOP_NUM
+                        # 重复多次尝试启动服务
+                        for ((LOOP_NUM=1;LOOP_NUM<4;LOOP_NUM++)); do
+                            sleep ${LOOP_NUM}s;
+                            if handle_run "$1" status >/dev/null;then
+                                warn_msg "服务还在继续，尝试强制关闭"
+                                run_msg kill -9 $RUN_PID
+                            else
+                                break
+                            fi
+                        done
                     fi
-                    ((LOOP_NUM--))
-                done
+                fi
+                if handle_run "$1" status >/dev/null;then
+                    error_exit "服务停止失败"
+                else
+                    info_msg "服务停止成功"
+                fi
+            else
+                warn_msg "服务未运行，无需关闭"
             fi
         ;;
         status)
             # 获取状态命令
-            if get_conf RUN_COMMAND "$1" "status-run"; then
-                run_msg "$RUN_COMMAND"
+            if has_conf "$1" "status-run";then
+                run_command "$1" "status-run"
             else
                 # 获取进程PID命令
-                local PID_NUM
-                get_handle_pid PID_NUM "$1"
-                info_msg "服务运行中"
+                local RUN_PID
+                if get_handle_pid RUN_PID "$1";then
+                    info_msg "服务运行中"
+                    return 0
+                fi
+                warn_msg "服务未运行"
+                return 1
             fi
         ;;
         *)
@@ -129,20 +162,25 @@ handle_run(){
 # return 0|1
 get_handle_pid(){
     # 获取进程PID命令
-    local PID_NUM
-    if get_conf RUN_COMMAND "$1" "pid-run"; then
-        PID_NUM=$(eval "$RUN_COMMAND")
+    local _PID
+    if has_conf "$2" "pid-run";then
+        _PID=$(run_command "$2" "pid-run"|tail -n 1)
     else
         # 获取进程PID文件
         local PID_FILE
-        if get_conf PID_FILE "$1" "pid-file" && [ -n "$PID_FILE"  -a -e "$PID_FILE" ];then
-            PID_NUM=$(cat $PID_FILE)
+        if get_conf PID_FILE "$2" "pid-file";then
+            if [ -n "$PID_FILE"  -a -e "$PID_FILE" ];then
+                _PID=$(cat $PID_FILE)
+            fi
+        else
+            error_exit "未配置PID获取"
         fi
     fi
-    if [[ "$PID_NUM" =~ ^[1-9][0-9]*$ ]] && [ ! -d "/proc/$PID_NUM/" ];then
-        error_exit "无法获取服务 ${1} 的PID"
+    if [[ "$_PID" =~ ^[1-9][0-9]*$ && -d "/proc/$_PID/" ]];then
+        eval "$1=$_PID"
+        return 0
     fi
-    eval "$1=$PID_NUM"
+    return 1
 }
 
 if ! [[ "$ARGU_action" =~ ^(start|restart|stop|status)$ ]];then
